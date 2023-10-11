d41b53f80ab2e8212538443d57e472e6517a909f
Maleiyah <maleiyahdavis@yahoo.com>
2023-10-11 10:02:52 -0500

    home, navbar components 10/11

--- frontend-app/forum-react-app/src/App.js
+++ frontend-app/forum-react-app/src/App.js
@@ -1,25 +1,31 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react"; 
+import {Routes,Route } from "react-router-dom";
+import Home from "./components/home/Home";
+import Navbar from "./components/navbar/Navbar";
+
+
+
+
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
 
-export default App;
+  
+    return (
+    
+        <div className="App">
+
+          
+            <Navbar/>
+            <Routes>
+              <Route path="/" exact element={<Home/>}/>
+              
+              
+              {/* <Route path="/posts" exact element={<Posts/>}/> */}
+              
+            </Routes>
+           
+          
+          
+        </div>)
+}
+export default App; 
